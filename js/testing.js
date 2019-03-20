aafe57f57448f172da17657e3686f6be7d93eb94
Jorge Alvarado <jalvaradoas39@outlook.com>
2019-03-20 14:57:39 -0400

    recursion

--- js/testing.js
+++ js/testing.js
@@ -1,1 +1,88 @@
-console.log('hello');
+/*
+	***** RECURSION *****
+	- What is recursion?
+		a function that calls itself
+	- How do they work?
+		invoking same function with different input until base case reached
+	- What two essential parts for recursive functions?
+		1) base case (the condition to end recursion)
+		2) different input (each time function called)
+	- Why use recursion?
+		faster and cleaner when using tree-like structures (i.e binary search trees, mergesort, quicksort, seaching file structures)
+*/
+
+function countDown1(num) {
+	if (num <= 0) {
+		console.log('Done!');
+		return;
+	}
+	console.log(num);
+	num--;
+	countDown1(num);
+}
+countDown1(3);
+
+
+
+function countDown2(num) {
+	for (let i = num; i > 0; i--) {
+		console.log(i);
+	}
+	console.log('Done!');
+}
+countDown2(3);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
